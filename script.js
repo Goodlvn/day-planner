9887e0426d0856d0021beadfeedbdf1599653ef3
JD Martinez <focus4ursoul@gmail.com>
2020-09-25 23:08:40 -0700

    load agenda from local storage and add saveBtn effects

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
-
-
-console.log(moment().format());
-
 var currentHour = parseInt(moment().format('HH'));
 
 function createPlanner(){
      
-    i = 0;
+    var i = 0;
 
-    iT = 9;
+    var iT = 9;
 
     while(i <= 8){
         var newRow = $("<div>");
@@ -22,7 +18,6 @@ function createPlanner(){
     
         var newTextCol = $("<textarea>");
         newTextCol.attr("class", "col-10 textarea");
-        newTextCol.attr("data-text", iT);
 
         var newSaveCol = $("<div>");
         newSaveCol.attr("class", "col-1 saveBtn text-center");
@@ -30,43 +25,52 @@ function createPlanner(){
 
         newLockImg = $("<img>");
         newLockImg.attr("src", "assets/images/locked.svg");
-        newLockImg.attr("class", "w-50 mt-4" )
+        newLockImg.attr("class", "w-50 mt-4")
 
         switch (iT) {
             case 9:
                 newHourCol.text(iT + "am");
+                newTextCol.attr("id", iT);
                 iT++;
             break;
             case 10:
                 newHourCol.text(iT + "am");
+                newTextCol.attr("id", iT);
                 iT++;
             break;
             case 11:
                 newHourCol.text(iT + "am");
+                newTextCol.attr("id", iT);
                 iT++;
             break;
             case 12:
                 newHourCol.text(iT + "pm");
+                newTextCol.attr("id", iT);
                 iT = iT - 11;
             break;
             case 1:
                 newHourCol.text(iT + "pm");
+                newTextCol.attr("id", iT);
                 iT++;
             break;
             case 2:
                 newHourCol.text(iT + "pm");
+                newTextCol.attr("id", iT);
                 iT++;
             break;
             case 3:
                 newHourCol.text(iT + "pm");
+                newTextCol.attr("id", iT);
                 iT++;
             break;
             case 4:
                 newHourCol.text(iT + "pm");
+                newTextCol.attr("id", iT);
                 iT++;
             break;
             case 5:
                 newHourCol.text(iT + "pm");
+                newTextCol.attr("id", iT);
                 iT++;
             break;
         }
@@ -79,8 +83,6 @@ function createPlanner(){
             newTextCol.addClass("future");
         }
 
-        
-
         $(".container").append(newRow);
         newRow.append(newHourCol, newTextCol, newSaveCol);
         newSaveCol.append(newLockImg);
@@ -89,22 +91,27 @@ function createPlanner(){
     }
 }
 
-function loadSavedItems(){
-    console.log(localStorage.getItem("9am"));
 
-    
-}
+function loadSavedItems(){
+    $("#9").val(localStorage.getItem("9am"));
+    $("#10").val(localStorage.getItem("10am"));
+    $("#11").val(localStorage.getItem("11am"));
+    $("#12").val(localStorage.getItem("12pm"));
+    $("#1").val(localStorage.getItem("1pm"));
+    $("#2").val(localStorage.getItem("2pm"));
+    $("#3").val(localStorage.getItem("3pm"));
+    $("#4").val(localStorage.getItem("4pm"));
+    $("#5").val(localStorage.getItem("5pm"));  
+};
 
 createPlanner();
+loadSavedItems();
 
 $(".saveBtn").on("click", function(){
-    $(this).attr("style", "background-color:green;");
 
     var timeStamp = $(this).siblings(".hour").text();
     var textValue = $(this).siblings(".textarea").val();
-
     localStorage.setItem(timeStamp, textValue);
-
 });
 
 
